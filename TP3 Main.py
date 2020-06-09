bc2cd3f96968dc4db03e2cfb3afec90376d4f092
AxelLarrea <axel.larrea.9@gmail.com>
2020-06-09 19:00:52 -0300

    09/06/20

--- TP3 Main.py
+++ TP3 Main.py
@@ -165,20 +165,93 @@ while not (en_frente(cola) == c):
 
 for i in range (0, tamanio(cola)):
     print(mover_final(cola))
-'''
+
 #Ejercicio 8 TERMINAR
 
 cola = Cola()
+cola2 = Cola()
 
-x = int(input('Ingrese un número: '))
-arribo(cola, x)
+
 while (tamanio(cola) < 10):
-    x = int(input('Ingrese un número: '))
-    if (x > (en_frente(cola))):
-        arribo(cola, x)
-    else:
-        arribo(cola, x)
-        mover_final(cola)
+    x = randint(0, 99)
+    arribo(cola, x)
 
+c = atencion(cola)
+arribo(cola2, c)
+
+print('Elementos de Cola: ')
 for i in range (0, tamanio(cola)):
     print(mover_final(cola))
+
+# C1: 
+# C2: 33, 55, 99, 101
+
+while not cola_vacia(cola):
+    if (en_frente(cola) > en_frente(cola2)):
+        mover_final(cola)
+    elif (tamanio(cola) == 1):
+            dato = atencion(cola)
+            arribo(cola2, dato)
+    else:
+        x = atencion(cola)
+        arribo(cola2, x)
+        while not (en_frente(cola2) == c):
+            mover_final(cola2)
+        mover_final(cola2)  
+
+print('Elementos de Cola ordenado: ')
+for i in range (0, tamanio(cola2)):
+    print(mover_final(cola2))
+
+#Ejercicio 9
+
+cola = Cola()
+maximo = 0
+minimo = 0
+cant = 0
+
+while (tamanio(cola) < 10):
+    x = int(input('Ingrese un número entero:'))
+    arribo(cola, x)
+    if (x > maximo):
+        maximo = x
+    elif (x < minimo):
+        minimo = x
+        cant += 1
+
+rango = (maximo - minimo) 
+
+print('Los elementos de la cola son: ')
+for i in range (0, tamanio(cola)):
+    print(mover_final(cola))
+print()
+
+print('El rango es:', rango)
+print('La cantidad de números negativos es:', cant)
+'''
+
+#Ejercicio 10
+
+class starwars(object):
+    def __init__(self):
+        self.nombre = ''
+        self.planeta = ''
+    
+    def __str__(self):
+        return self.nombre + " - " + self.planeta
+
+
+cola = Cola()
+SW = starwars()
+
+while (tamanio(cola) < 5):
+    SW.nombre = input('Ingrese nombre del personaje: ')
+    SW.planeta = input('Ingrese el planeta del personaje: ')
+    arribo(cola, SW)
+    print()
+
+print('Los elementos de la cola son: ')
+
+while not cola_vacia(cola):
+    x = atencion(cola)
+    print(x)
