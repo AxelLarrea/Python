d53a056a946a279c5aa69ff665ffe809d19fe50e
Axel Larrea <63266736+AxelLarrea@users.noreply.github.com>
2020-11-03 23:15:31 -0300

    03-11-2020

--- TP1 Main.py
+++ TP1 Main.py
@@ -1,18 +1,18 @@
 '''
 #Ejercicio 1 "Fibonacci"
 
-n = int(input('Ingrese cuantos números de la sucesión quiere ver: '))
+# n = int(input('Ingrese cuantos números de la sucesión quiere ver: '))
 
-def Fibonacci(num):
-    a = 0
-    b = 1
-    i = 0
-    for i in range(0, num):
-        if(num > i):
-            a, b = b, a+b
-            print(a, b)
+# def Fibonacci(num):
+#     a = 0
+#     b = 1
+#     i = 0
+#     for i in range(0, num):
+#         if(num > i):
+#             a, b = b, a+b
+#             print(a, b)
 
-Fibonacci(n)
+# Fibonacci(n)
 
 
 def FibonacciRec(num):
@@ -27,19 +27,19 @@ print(FibonacciRec(num))
 
 #Ejercicio 2
 
-n = int(input('Ingrese hasta cuanto quiere sumar: '))
+# n = int(input('Ingrese hasta cuanto quiere sumar: '))
 
-def sumatoria(num):
-    i = 0
-    suma = 0
-    if(num <= 0):
-        print('Tiene que ser un número positivo')
-    else:
-        for i in range(0, num+1):
-            suma += i
-            print('La sumatoria es: ', suma)
+# def sumatoria(num):
+#     i = 0
+#     suma = 0
+#     if(num <= 0):
+#         print('Tiene que ser un número positivo')
+#     else:
+#         for i in range(0, num+1):
+#             suma += i
+#             print('La sumatoria es: ', suma)
 
-sumatoria(n)
+# sumatoria(n)
 
 num = int(input('Ingrese un número positivo, para realizar la sumatoria: '))
 
@@ -54,13 +54,13 @@ print(SumatoriaRec(num))
 
 #Ejercicio 3
 
-n1 = int(input('Ingrese su primer número: '))
-n2 = int(input('Ingrese su segundo número: '))
+# n1 = int(input('Ingrese su primer número: '))
+# n2 = int(input('Ingrese su segundo número: '))
 
-def producto(num1, num2):
-    print('El producto es: ', num1 * num2)
+# def producto(num1, num2):
+#     print('El producto es: ', num1 * num2)
 
-producto(n1, n2)
+# producto(n1, n2)
 
 def ProductoRec(num1, num2):
     if (num1 == 0 or num2 == 0):
@@ -75,10 +75,10 @@ print('El producto es: ', ProductoRec(n1, n2))
 n1 = int(input('Ingrese su primer número, el cual será la base: '))
 n2 = int(input('Ingrese su segundo número, el cual será el exponente: '))
 
-def potencia(num1, num2):
-    print('La potencia es: ', num1 ** num2)
+# def potencia(num1, num2):
+#     print('La potencia es: ', num1 ** num2)
 
-potencia(n1, n2)
+# potencia(n1, n2)
 
 def PotenciaRec(num1, num2):
     if(num1 == 1  or num2 == 0):
@@ -208,7 +208,6 @@ print('La suma de los dígitos es:', sumdigRec(n))
 #Ejercicio 14
 
 num = int(input('Ingrese el número entero a radicar: '))
-mult = 1
 
 def raizRec(num, mult):
     if (mult * mult) <= num :
@@ -260,10 +259,14 @@ for i in range(0, len(mat)):
 def barrido_matriz(mat, i, j):
     if(i<len(mat) and j<len(mat[i])):
         print(mat[i][j])
+    else:
         if(j == len(mat[i])-1):
-            i += 1
-            j = -1
-        barrido_matriz(mat, i, j+1)
+            print(mat[i][j])
+            j = 0
+            barrido_matriz(mat, i+1, j)
+        else:
+            print(mat[i][j])
+            barrido_matriz(mat, i, j+1)
 
 for i in range(0, len(mat)):
     for j in range(0, len(mat[i])):
@@ -281,6 +284,137 @@ def sucesionRec(num):
 
 print('El valor de la sucesion es:', sucesionRec(num))
 
+
+
+
+#Ejercicio 19
+
+vec = [90, 87, 13, 1, 15, 3, 17, 21]
+pos = 0
+
+def busqueda(vec, pos, buscado):
+    if pos == len(vec):
+        return 'El elemento no está en la lista'
+    elif (vec[pos] == buscado):
+        return 'El elemento está en la lista'
+    else:
+        return busqueda(vec, pos+1, buscado)
+
+buscado = int(input('Ingrese número a buscar'))
+
+print(busqueda(vec, pos, buscado))
+
+
+
+#Ejercicio 20
+
+vec = [1, 3, 4, 8, 9, 10, 20, 21]
+pos = 0
+
+def bbin_rec(vec, buscado, pos, ult):
+    if pos < ult:
+        med = (pos+ult)//2
+        if vec[med] == buscado:
+            return med
+        else:
+            if vec[med] > buscado:
+                return bbin_rec(vec, buscado, pos, med-1)
+            else:
+                return bbin_rec(vec, buscado, med+1, ult)
+
+x = bbin_rec(vec, 2, pos, len(vec))
+if x is not None:
+    print('El elemento está en la lista, es:', vec[x])
+else:
+    print('El elemento no está en la lista')
+
+
+
+#Ejercicio 21
+
+troopers = [["20° 13' 22'' ", 'Cañón', 'Derribado'], ["37° 20' 53'' ", 'Blaster', 'No derribado'], ["50° 14' 33'' ", 'Cañón doble', 'Derribado']]
+aux = []
+pos = 0
+
+def derribado(vec, aux, pos):
+    if (pos == len(vec)):
+        return aux
+    elif(vec[pos][2] == 'Derribado'):
+        aux += vec[pos]
+        return derribado(vec, aux, pos+1)
+    else:
+        return derribado(vec, aux, pos+1)
+
+print(derribado(troopers, aux, pos))
+
+
+#Ejercicio 22
+
+def matriz_laberinto(i, j, mat):
+    tam = len(mat)
+    if i == tam-1 and j == tam-1:
+        return 'Salio del laberinto'
+    else:
+        if i == tam-1:
+            if mat[i][j+1] == 0:
+                print('derecha')
+                return matriz_laberinto(i, j+1, mat)
+            else:
+                if mat[i-1][j] == 0:
+                    print('arriba')
+                    mat[i][j] = 1
+                    return matriz_laberinto(i-1, j, mat)
+        else:
+            if j == tam-1:
+                if mat[i+1][j] == 0:
+                    print('abajo')
+                    return matriz_laberinto(i+1, j, mat)
+                else:
+                    if mat[i][j-1] == 0:
+                        print('izquierda')
+                        mat[i][j] = 1
+                        return matriz_laberinto(i, j-1, mat)
+            else:
+                if mat[i+1][j] == 0:
+                    print('arriba')
+                    return matriz_laberinto(i+1, j, mat)
+                else:
+                    if mat[i][j+1] == 0:
+                        print('derecha')
+                        return matriz_laberinto(i, j+1, mat)
+                    else:
+                        # mat[i][j] = 1
+                        if mat[i-1][j] == 0:
+                            print('arriba')
+                            return matriz_laberinto(i-1, j, mat)
+
+mat = [[0,1,1], 
+       [0,1,0], 
+       [0,0,0], 
+       [1,1,0]]
+
+matriz_laberinto(0, 0, mat)
+
+
+'''
+#Ejercicio 23
+
+def torre_hanoi(discos, aguja1, aguja2, aguja3, cont):
+    if (discos <= 0):
+        print('Listo. La cantidad de movimientos fueron:', cont)
+    else:
+        torre_hanoi(discos-1, aguja1, aguja3, aguja2, cont+1)
+        #print('Se mueve de aguja1 a aguja3')
+        torre_hanoi(discos-1, aguja2, aguja1, aguja3, cont+1)
+
+cont = 0
+aguja1 = 10
+aguja2, aguja3 = 0, 0
+discos = 10
+torre_hanoi(discos, aguja1, aguja2, aguja3, cont)
+print('A1:', aguja1,'A2:', aguja2, 'A3:', aguja3)
+
+'''
 #Ejercicio 24
 
 termino = int(input('Ingrese el término de la sucesión que desee ver: '))
